6dd75354267bfe1a6bc648b576ba218105834ac7
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-11-04 23:54:03 +0100

    Update scanimage.sh

--- scanimage.sh
+++ scanimage.sh
@@ -3,6 +3,8 @@
 curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
 chmod a+x ./twistcli
 
+docker pull itresoldi/evilpetclinic:latest
+
 ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details itresoldi/evilpetclinic -o output.json
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
 
