c5b99cc9903896483e33a26ad0fa1779b27fa43b
ivan-tresoldi <67233434+ivan-tresoldi@users.noreply.github.com>
2021-11-04 22:49:44 +0100

    Create scanimage.sh

---
+++ scanimage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -k -u $TL_USER:$TL_PASS --details --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
+chmod a+x ./twistcli
+
+./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details itresoldi/evilpetclinic -o output.json
+result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
+
+echo
+
+if [ -z "$result" ]; then
+   echo "Image Vulnerability scan passed!"
+   exit 0
+else
+   echo "Image Vulnerability scan failed!"
+   exit 1
+fi
